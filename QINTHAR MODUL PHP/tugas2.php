6bb59d696b8855338e8e05a34581c0d19cd27a7f
Muhammad Qinthar <114649475+qintharganteng@users.noreply.github.com>
2022-12-21 23:27:43 +0700

    Add files via upload

---
+++ QINTHAR MODUL PHP/tugas2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS 2</title>
+</head>
+<body>
+    <h2>TUGAS 2</h2>
+    Hasil Pejumlahan Bilangan Genap Dari 0 S/d 6 adalah
+    <?php
+    $num=0;
+    $a=0;
+    while($a <=6);
+    {
+        $num +=$a;
+        $a +=2;
+    }
+    echo $num
+    ?>
+</body>
+</html>
